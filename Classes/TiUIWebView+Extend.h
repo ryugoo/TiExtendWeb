98e836e706a9e47dc1dc60a79fa12a8ff939ec58
Ryutaro Miyashita <ryugoo@gmail.com>
2014-02-08 02:00:00 +0900

    Cleanup

--- Classes/TiUIWebView+Extend.h
+++ Classes/TiUIWebView+Extend.h
@@ -1,16 +1,11 @@
-//
-//  TiUIWebView+Extend.h
-//  TiExtendWeb
-//
-//  Created by ryugoo on 2013/05/18.
-//
-//
-
 #import "TiBase.h"
 #import "TiUIWebView.h"
 
 @interface TiUIWebView (TiUIWebView_Extend)
 
 - (void)setNormalScrollSpeed_:(id)args;
+- (void)setRemoveShadow_:(id)args;
+- (void)setRemoveScrollDelay_:(id)args;
+- (void)setUserAgentForiOS_:(id)args;
 
 @end
--- Classes/TiUIWebView+Extend.m
+++ Classes/TiUIWebView+Extend.m
@@ -1,13 +1,4 @@
-//
-//  TiUIWebView+Extend.m
-//  TiExtendWeb
-//
-//  Created by ryugoo on 2013/05/18.
-//
-//
-
 #import "TiUIWebView+Extend.h"
-#import "TiUIWebViewProxy.h"
 #import "TiUtils.h"
 
 @implementation TiUIWebView (TiUIWebView_Extend)
@@ -15,12 +6,10 @@
 - (void)setNormalScrollSpeed_:(id)args
 {
     BOOL normalSpeed = [TiUtils boolValue:args def:NO];
-    UIScrollView *scrollView = [self performSelector:@selector(scrollview) withObject:nil];
+    UIScrollView *scrollView = [self performSelector:@selector(scrollview)];
     if (normalSpeed == YES) {
-        // Same TableView, ScrollView scroll speed.
         [scrollView setDecelerationRate:UIScrollViewDecelerationRateNormal];
     } else {
-        // Default scroll speed.
         [scrollView setDecelerationRate:UIScrollViewDecelerationRateFast];
     }
 }
@@ -28,7 +17,7 @@
 - (void)setRemoveShadow_:(id)args
 {
     BOOL removeFlag = [TiUtils boolValue:args def:NO];
-    UIScrollView *scrollView = [self performSelector:@selector(scrollview) withObject:nil];
+    UIScrollView *scrollView = [self performSelector:@selector(scrollview)];
     if (removeFlag == YES)
     {
         for (UIView *shadowView in [scrollView subviews]) {
@@ -42,7 +31,7 @@
 - (void)setRemoveScrollDelay_:(id)args
 {
     BOOL removeFlag = [TiUtils boolValue:args def:NO];
-    UIScrollView *scrollView = [self performSelector:@selector(scrollview) withObject:nil];
+    UIScrollView *scrollView = [self performSelector:@selector(scrollview)];
     if (removeFlag == YES)
     {
         [scrollView setDelaysContentTouches:NO];
@@ -51,34 +40,24 @@
 
 - (void)setUserAgentForiOS_:(id)args
 {
-    NSDictionary *dict = @{@"UserAgent": args};
-    [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:@{@"UserAgent": args}];
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
-    // Get request URL
     NSURL *requestURL = [request URL];
-    // Get scheme information
     NSString *scheme = [[requestURL scheme] lowercaseString];
-    // Get URL path information
     NSString *path = [requestURL path];
-    // If the path begins with "/"
     if([path hasPrefix:@"/"]) {
-        // Remove the head of the "/"
         path = [path substringWithRange:NSMakeRange(1, path.length - 1)];
     }
-    // If the scheme begins with "extendwebview"
     if ([scheme isEqualToString:@"extendwebview"]) {
-        // Get event name from host
         NSString *eventName = [requestURL host];
-        // The same as the event name if it has been registered in the event listener
         if ([self.proxy _hasListeners:eventName]){
             NSLog(@"[DEBUG] fire: %@",eventName);
-            // Get event information
-            NSDictionary *event = [NSDictionary dictionaryWithObjectsAndKeys:path, @"path", eventName, @"event", nil];
-            // Execute
-            [self.proxy fireEvent:eventName withObject:event];
+            [self.proxy fireEvent:eventName
+                       withObject:@{@"path": path,
+                                    @"event": eventName}];
         }
         return NO;
     }
